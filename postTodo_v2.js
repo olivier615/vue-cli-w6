0d373b852d15fb530df8395a0efcc5ed95ad0685
huang-hua1212 <hahahazzz123@gmail.com>
2022-03-25 14:56:07 +0800

    update postTod_v2.js

--- postTodo_v2.js
+++ postTodo_v2.js
@@ -1,15 +1,20 @@
 // const errorHandle = require("./errorHandle");
 
 function postTodo(res, todos){
-    const title = JSON.parse(res.status(200).body).title;
-    if(title === undefiend){
-        errorHandle(res);
+    // const title = JSON.parse(res.status(200).body).title;
+    const title = JSON.parse(req.body).title;
+    if(JSON.parse(req.body).title === undefiend){
+        errorHandle(res.status(200));
     }else{
-        const obj = {
+        // const obj = {
+        //     "status": "success",
+        //     "data": todos,
+        // }
+        // res.status(200).json(obj);
+        res.status(200).json({
             "status": "success",
             "data": todos,
-        }
-        res.json(obj);
+        });
     }
 
 }
