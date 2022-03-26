62f6768458c4ebe8572e24b27d685abf659b4ed7
huang-hua1212 <hahahazzz123@gmail.com>
2022-03-26 13:30:12 +0800

    update getTodo、postTodo

--- getTodo_v2.js
+++
@@ -1,9 +0,0 @@
-function getTodo(res, todos){
-    res.status(200).json({
-        status: 'success',
-        data: todos,
-    })
-}
-
-
-module.exports = getTodo
---
+++ src/postTodo.js
@@ -0,0 +1,18 @@
+const { v4: uuidv4 } = require('uuid');
+function postTodo(res, req, todos){
+    const title = JSON.parse(req.body).title;
+    if(title === undefined){
+        res.status(200).json({ status: 'false', message: "欄位未填寫正確，或無此 todo ID" })
+    }else{
+        todos.push({
+            "title": title,
+            "id": uuidv4(),
+        })
+        res.status(200).json({
+            "status": "success",
+            "data": todos,
+        });
+    }
+}
+
+module.exports = postTodo
